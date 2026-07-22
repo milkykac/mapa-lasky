a647ea7b614ffa6e306525e32134e618101ee25b
milkykac <mkacvi@gmail.com>
2026-07-22 20:17:47 +0200

    fix: add location permissions for geolocation in Android APK
    - Added ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION to AndroidManifest.xml
    - Added runtime permission request in MainActivity.onCreate()
    - Fixes 'Use my location' button not working in Capacitor Android app

--- android/app/src/main/java/com/martinadka/mapalasky/MainActivity.java
+++ android/app/src/main/java/com/martinadka/mapalasky/MainActivity.java
@@ -1,5 +1,29 @@
 package com.martinadka.mapalasky;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private static final int LOCATION_PERMISSION_REQUEST = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Request location permissions on startup so navigator.geolocation works in WebView
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                },
+                LOCATION_PERMISSION_REQUEST);
+        }
+    }
+}
